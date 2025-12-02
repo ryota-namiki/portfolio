e9132e083891a07dd8a965eb2aab5b964d7da54c
並木遼太 <ryota@namikiryoufutoshinoMacBook-Pro.local>
2025-12-02 14:39:10 +0900

    liquid glass追加

--- header.php
+++ header.php
@@ -17,6 +17,16 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <defs>
+    <filter id="glass-distortion" x="0%" y="0%" width="100%" height="100%">
+      <feTurbulence type="fractalNoise" baseFrequency="0.008 0.008" numOctaves="2" seed="92" result="noise" />
+      <feGaussianBlur in="noise" stdDeviation="2" result="blurred" />
+      <feDisplacementMap in="SourceGraphic" in2="blurred" scale="77" xChannelSelector="R" yChannelSelector="G" />
+    </filter>
+  </defs>
+</svg>
+
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
 		<nav class="navbar">
